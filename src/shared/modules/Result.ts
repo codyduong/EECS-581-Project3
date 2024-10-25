500a2d3c2bcaccc10af0bf8e895b0a6b297a8348
Cody Duong <duongcody507@gmail.com>
2024-10-24 21:40:57 -0500

    saving work

--- src/shared/modules/Result.ts
+++ src/shared/modules/Result.ts
@@ -1,3 +1,8 @@
+/**
+ * @author Cody Duong <cody.qd@gmail.com>
+ * @file Result abstract class inspired by Rust
+ */
+
 export default abstract class Result<T, E> {
   protected value: T | E;
   public constructor(value: T | E) {
